4151e0aa1f23b25c04b59a70e75ee9f80197fa0d
sanjidanisha <snisha000@citymail.cuny.edu>
2021-02-25 23:59:35 -0500

    fixster part 2

---
+++ fixster/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  fixster
+//
+//  Created by Sanjida Ayman Nisha on 2/25/21.
+//  Copyright © 2021 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
